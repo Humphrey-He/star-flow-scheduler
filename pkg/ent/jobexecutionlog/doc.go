da85c375be0b618313f29079b119692d6e7955fe
hexiefeng <hexiefeng@zhuque.email>
2026-03-26 16:49:39 +0800

    chore(ent): add generated dead letter log and shard code

---
+++ pkg/ent/jobexecutionlog/doc.go
@@ -0,0 +1,3 @@
+// Package jobexecutionlog is a stub package to satisfy module resolution.
+// Real ent-generated code should replace this.
+package jobexecutionlog
---
+++ pkg/ent/jobshard/doc.go
@@ -0,0 +1,3 @@
+// Package jobshard is a stub package to satisfy module resolution.
+// Real ent-generated code should replace this.
+package jobshard
